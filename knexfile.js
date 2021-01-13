355b0784e0474aec026b0052be64c3184feb0b5e
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-01-13 10:38:42 -0800

    updating dev connection config

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,51 @@
-// Update with your config settings.
+var pg = require("pg");
+
+const sharedConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+
+  pool: {
+    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+  },
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
+};
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    ...sharedConfig,
+    connection: { filename: "./data/Simpado.db3" },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
