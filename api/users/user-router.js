2d28b3e2ca83ba26e2db10e1573de9b9fb051f72
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-01-13 22:15:57 -0800

    Added validation, user can only get own profile

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const Helper = require("./user-model");
+const { validateUserId } = require("./user-middleware");
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const [user] = await Helper.findByID(id);
-    if (user.length <= 0) {
-      res
-        .status(200)
-        .json({ message: "We were unable to locate the requested user." });
-    } else {
-      user.password = null;
-      let success = { ...user, subscriptions: ["these come later."] };
-      res.status(200).json(success);
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+router.get("/:id", validateUserId, (req, res) => {
+  let user = req.User;
+  let success = { ...user, subscriptions: ["these come later."] };
+  res.status(200).json(success);
 });
 
 module.exports = router;
