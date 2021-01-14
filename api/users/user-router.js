d5d5092fcb40b844db04db9a88c25b93ba3ee336
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-01-13 22:19:23 -0800

    Updated error message 'Access Denied'

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -4,9 +4,14 @@ const { validateUserId } = require("./user-middleware");
 const router = express.Router();
 
 router.get("/:id", validateUserId, (req, res) => {
+  const { id } = req.params;
   let user = req.User;
-  let success = { ...user, subscriptions: ["these come later."] };
-  res.status(200).json(success);
+  if (user.id != id) {
+    res.status(400).json({ message: "Access Denied." });
+  } else {
+    let success = { ...user, subscriptions: ["these come later."] };
+    res.status(200).json(success);
+  }
 });
 
 module.exports = router;
